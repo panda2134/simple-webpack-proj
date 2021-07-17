2792de294bf43a2568028817dc28318154081bbf
panda2134 <ljypanda@live.com>
2021-07-17 10:28:16 +0800

    build: loaders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import confetti from 'canvas-confetti'
 import delay from 'delay'
+import './assets/main.css'
 
 async function main() {
 	const hint = document.getElementById('hint')
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/i, // .js / .mjs
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // use babel for compatibility
+          options: {
+            presets: [
+              ['@babel/preset-env', { // https://babeljs.io/docs/en/babel-preset-env
+                useBuiltIns: 'usage', // import used polyfills in core-js automatically
+                corejs: '3',
+                targets: {
+                  chrome: '41',
+                  ie: '9' // and Promise polyfill will be introduced
+                }
+              }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i, // allows you to import css
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
